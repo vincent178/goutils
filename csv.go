54d8e534112a47ee98c9a57892547ca5f4cd6aea
vincent178 <vh7157@gmail.com>
2020-08-19 20:58:06 +0800

    add readcsv

---
+++ csv.go
@@ -0,0 +1,51 @@
+package goutils
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+)
+
+func ReadCsv(f io.Reader) <-chan map[string]string {
+	r := csv.NewReader(f)
+	r.ReuseRecord = true
+	r.TrimLeadingSpace = true
+
+	ch := make(chan map[string]string)
+
+	go func() {
+		defer close(ch)
+
+		withHeader := true
+		var headers []string
+
+		for {
+			record, err := r.Read()
+			if err != nil {
+				if err == io.EOF {
+					log.Println("finish read csvutil file")
+					return
+				} else {
+					log.Fatal(err)
+					return
+				}
+			}
+			if withHeader {
+				headers = make([]string, len(record))
+				copy(headers, record)
+				withHeader = false
+				continue
+			}
+
+			data := map[string]string{}
+
+			for idx, header := range headers {
+				data[header] = record[idx]
+			}
+
+			ch <- data
+		}
+	}()
+
+	return ch
+}
